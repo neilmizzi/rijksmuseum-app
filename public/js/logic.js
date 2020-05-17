a62d3f08ece49d9dedc53a233a8732d089daf7f2
neilmizzi <mizzin@hotmail.com>
2020-05-18 01:38:42 +0200

    added rijksdata API call example

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,6 +1,27 @@
-$('#sendMsgBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
+$('#sendMsgBtn').click(exampleAPICall); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
 
 function exampleAlert() {
 	alert("Author: " + $('#author').val() + "\nMessage: " + $('#message').val());
 	console.log("Button pressed!");
+}
+
+function exampleAPICall() {  
+	// NB: pass params depending on API documentation at https://data.rijksmuseum.nl/object-metadata/api/
+	// Replace [apikey] with your own API key
+	let url = "https://www.rijksmuseum.nl/api/nl/collection?key=BGFhMFQa&involvedMaker=Rembrandt+van+Rijn";
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", url, true);
+
+	xmlhttp.send();
+	xmlhttp.addEventListener("readystatechange", processRequest, false);
+
+	function processRequest(e) {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			json_file = JSON.parse(xmlhttp.responseText);
+
+			// Whatever your heart and soul desires for the JSON file, has to be done in this block below
+			alert(JSON.stringify(json_file));
+		}
+	}
+	
 }
