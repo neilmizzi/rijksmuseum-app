6faffb69eae180755ae280365c08c26dffa81d47
neilmizzi <mizzin@hotmail.com>
2020-06-02 14:59:55 +0200

    test

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,8 +1,9 @@
-$('#sendMsgBtn').click(exampleAPICall); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
+$('#sendMsgBtn').click(exampleAlert); // Collect HTML element with (#) id sendMsgBtn, when click do function exampleAlert
 
 function exampleAlert() {
 	alert("Author: " + $('#author').val() + "\nMessage: " + $('#message').val());
 	console.log("Button pressed!");
+	console.log("test");
 }
 
 function exampleAPICall() {  
